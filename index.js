0a333cfec54b859a7f0334da91b575738126125b
Smith Huamani Hilario <>
2020-07-09 01:24:30 -0500

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+//Core Node (fs, http, events ...)
+//utilizar archivos modulos
+//Paquetes instalados con NPM
+
+console.log('Hola mundo'.yellow); // outputs green text
